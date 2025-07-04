6742bb8cef75579d9b1f0e7cdd8bea9c9c2505fa
neo-edu2025 <neoeduprojeto2025@gmail.com>
2025-07-04 12:10:32 -0300

    Projeto-NEOEDU

---
+++ NEO EDU - PROJETO/Principal/script.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.efeitoScroll', { duration: 1000 });
+
+sr.reveal('.efeitoScroll2', {
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 3350
+ });
